42afb3997347c095f2afde8157dc11b7d85b42b9
odobok <katrina.kennett@gmail.com>
2018-04-25 19:15:19 -0600

    adding ejs

--- node-code-along/server.js
+++ node-code-along/server.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
 
-app.get('/howdy', function(req, res) {
+app.get('/', function(req, res) {
+  res.render('index');
+})
+
+app.get('/howdy', function (req, res) {
   res.end('We made it here. Howdy!');
 });
 
-var server = app.listen(5000, function() {
-  console.log('Server fired up on Port 5000')
+app.get('/cheer', function(req, res) {
+  res.end('It is a wonderful evening');
+});
+
+app.get('/jeer', function(req, res) {
+  res.end('It is not the best evening');
+});
+
+
+
+var server = app.listen(5000, function () {
+  console.log('Server fired up on Port 5000');
 });
